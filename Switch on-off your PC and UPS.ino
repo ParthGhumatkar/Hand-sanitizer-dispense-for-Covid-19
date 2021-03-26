2c65eb400b1961e228196e629254c3d00ba40fdf
Parth Ghumatkar <parthghumatkar@gmail.com>
2021-03-26 10:00:34 +0530

    Update Switch on-off your PC and UPS.ino

--- Switch on-off your PC and UPS.ino
+++ Switch on-off your PC and UPS.ino
@@ -1,5 +1,7 @@
+#include <Servo.h>
+#include <RTClib.h> // for the RTC
 
-#include Servo.h
+RTC_DS1307 rtc;
 
 Servo servo1;
 Servo servo2;
@@ -7,12 +9,25 @@ Servo servo3;
 Servo servo4;
 Servo servo5;
 
+const int TurnONHour1 = 07;
+const int TurnONMinute1 =30;
+
+int CurrentHour;
+int CurrentMinute;
+
+const int startsystem = 2;
+
+
 char fba;      
 int pos1 = 0;    
 int pos2 = 0;
 int pos3 = 0;    
 int pos4 = 0;
 int po5 = 0;    
+int ONbutton = 0;
+int OFFbutton = 0;
+#define ONbuttonPin 4
+#define OFFbuttonPin 3
 
 void setup() {
   servo1.attach(9);
@@ -21,6 +36,9 @@ void setup() {
   servo4.attach(6);
   servo5.attach(5);
   Serial.begin(9600);
+
+  rtc.begin();
+  pinMode(startsystem, OUTPUT);
 }
 
 void TurnOnGrpservo4(){
@@ -68,6 +86,9 @@ delay(100);
 }
 
 void loop() {
+  ONbutton = digitalRead(ONbuttonPin);
+  OFFbutton = digitalRead(ONbuttonPin);
+  
   fba = Serial.read();
   if (fba == '1'){
     TurnOnGrpservo4();  
@@ -84,7 +105,35 @@ void loop() {
     delay(4000);
     TurnOffGrpservo5();
     delay(1000);
-    
+
   }
+    if (ONbutton == HIGH) {
+    TurnOnGrpservo4();  
+    delay(15000);
+    TurnOnGrpservo2();
+    delay(4000);
+    TurnOnGrpservo1();
+  } 
+  
+    if (OFFbutton == HIGH) {
+    TurnOnGrpservo1();
+    delay(20000);
+    TurnOffGrpservo3();
+    delay(4000);
+    TurnOffGrpservo5();
+    delay(1000);
+  } 
+  
+    DateTime now = rtc.now();
+    CurrentHour = now.hour();
+    CurrentMinute = now.minute();
+  Serial.println(CurrentHour);
+  Serial.println(CurrentMinute);
+  if((CurrentHour==TurnONHour1) && (CurrentMinute == TurnONMinute1)){
+    digitalWrite(startsystem,HIGH);
+    Serial.println("PC ON");
+    }
+    
+  
 
 }
